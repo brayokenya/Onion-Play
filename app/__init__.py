17b97dc8e38a78df235afe7e74c5e8f5065fc671
brayokenya <kiirubrian21@gmail.com>
2020-06-02 20:05:20 +0300

    added authentication

--- app/__init__.py
+++ app/__init__.py
@@ -30,5 +30,8 @@ def create_app(config_name):
     from .requests import configure_request
     configure_request(app)
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
     return app
 
--- manage.py
+++ manage.py
@@ -2,7 +2,8 @@ from app import create_app,db
 from app.models import User
 from app import create_app
 from flask_script import Manager,Server
-
+from app.models import User,Role
+from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('development')
@@ -10,9 +11,15 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
 
+
+
+#Flask-Migrate
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User, Role = Role )
 
 if __name__ == '__main__':
     manager.run()
