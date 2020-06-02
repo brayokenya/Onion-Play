1ea2b64764de078a02e9c1beee073c60a1fdcad6
brayokenya <kiirubrian21@gmail.com>
2020-06-02 11:44:11 +0300

    adding database

--- start.sh
+++ start.sh
@@ -2,4 +2,7 @@ export MOVIE_API_KEY='d882445304b469ab965a1ae1a487e8bf'
 
 export SECRET_KEY='d882445304b469ab965a1ae1a487e8bf'
 
+SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://briankiiru:andela@localhost/watchlist'
+
+
 python3.8 manage.py server
